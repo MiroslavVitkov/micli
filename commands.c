3c0839c24bbf8e2b7116831c37efa954253b5372
Miroslav Vitkov <sir.vorac@gmail.com>
2015-11-10 22:29:32 +0200

    Typing !reprogram invokes the bootloader in 3 seconds.

--- commands.c
+++ commands.c
@@ -1,7 +1,13 @@
 #include "usart.h"
 
+#include <util/delay.h>
+
 void cmd_reprogram(void)
 {
+    char msg[] = "Loading bootloader in 3 seconds.";
+    usart_transmit_block(sizeof(msg), msg);
+    _delay_ms(3000);
+
     // TODO: obviously extract that address somewhere.
     typedef void (* fn_ptr_t) (void);
     fn_ptr_t my_ptr = (void *)0x1E00;
@@ -12,12 +18,14 @@ void cmd_reprogram(void)
 typedef void (* fn_ptr_t) (void);
 typedef struct command
 {
-    fn_ptr_t handler;
+    const fn_ptr_t handler;
     const char *msg;
+    const unsigned len;           // Without the terminating '\0'.
 } command_t;
+#define DECLARE_COMMAND(name) {cmd_##name, #name, sizeof(#name)-1},
 const command_t Commands[] =
 {
-{cmd_reprogram, "reprogram"}
+    DECLARE_COMMAND(reprogram)
 };
 
 
@@ -31,14 +39,13 @@ void listen_for_command(void)
     while (usart_receive() != '!');
     for(int i = 0; i < sizeof(Commands) / sizeof(command_t); ++i)
     {
-        unsigned command_length = sizeof(Commands[i].msg);       // Without the terminating '\0'.
-        for(int j = 0; j < command_length; ++j)
+        for(int j = 0; j < Commands[i].len; ++j)
         {
             char in = usart_receive();
             if(in != Commands[i].msg[j]) break;                   // Continue to test next command.
-            if(j == command_length)
+            if(j == Commands[i].len-1)
             {
-                Commands[i].handler();
+                cmd_reprogram(); //Commands[i].handler();
                 return;
             }
         }
@@ -46,22 +53,8 @@ void listen_for_command(void)
 }
 
 
-#include <util/delay.h>
 void main(void)
 {
     usart_init();
-
-    char msg[] = "Loading bootloader in 3 seconds.";
-    usart_transmit_block(msg, sizeof(msg));
-    _delay_ms(3000);
-
-    typedef void (* fn_ptr_t) (void);
-    fn_ptr_t my_ptr = (void *)0x1800;
-    my_ptr();
-
-
-    //usart_init();
-    //while(1) usart_transmit('S');
-    //listen_for_command();
-    //return 0;
+    listen_for_command();
 }
