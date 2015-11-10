bdbccd75846821d2a3bd3a007fa7ab5e4c6c2282
Miroslav Vitkov <sir.vorac@gmail.com>
2015-11-10 18:59:18 +0200

    App calling the bootlaoder.

--- commands.c
+++ commands.c
@@ -1,1 +1,9 @@
-int main(){return 0;}
+void cmd_reprogram()
+{
+    // TODO: obviously extract that address somewhere.
+    typedef void (* fn_ptr_t) (void);
+    fn_ptr_t my_ptr = (void *)0x1E00;
+    my_ptr();
+}
+
+int main(){cmd_reprogram(); return 0;}
